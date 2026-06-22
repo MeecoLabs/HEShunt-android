366ace045052f0c25f6efcfbaf6532c905e59b21
Dustin Steiner <124467+alopix@users.noreply.github.com>
2026-06-22 16:50:41 +0100

    prepare theming

---
+++ app/src/main/java/eu/meecolabs/heshunt/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package eu.meecolabs.heshunt.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// HES Brand Colors
+val OffBlack = Color(0xFF1B181C)
+val HesVelvet = Color(0xFF4E1E66)
+val HesBlue = Color(0xFF005685)
+val HesGreen = Color(0xFF41A298)
+val HesGreenTint = Color(0xFFA4CAC6)
+val HesDurness = Color(0xFFF9BE00)
+val HesDurnessTint = Color(0xFFFFE48C)
+val HesEdinburghRed = Color(0xFFA61F29)
+val HesErrorRed = Color(0xFFDC2E2E)
+
+// App Icon Background
+val IconBackground = Color(0xFF002855)
---
+++ app/src/main/java/eu/meecolabs/heshunt/ui/theme/Theme.kt
@@ -0,0 +1,49 @@
+package eu.meecolabs.heshunt.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = HesDurnessTint,
+    onPrimary = OffBlack,
+    secondary = HesGreenTint,
+    onSecondary = OffBlack,
+    tertiary = HesDurness,
+    onTertiary = OffBlack,
+    background = OffBlack,
+    surface = OffBlack,
+    onBackground = Color.White,
+    onSurface = Color.White,
+    error = HesErrorRed,
+    onError = Color.White
+)
+
+private val LightColorScheme = lightColorScheme(
+    primary = HesVelvet,
+    onPrimary = Color.White,
+    secondary = HesBlue,
+    onSecondary = Color.White,
+    tertiary = HesGreen,
+    onTertiary = Color.White,
+    background = Color.White,
+    surface = Color.White,
+    onBackground = OffBlack,
+    onSurface = OffBlack,
+    error = HesErrorRed,
+    onError = Color.White
+)
+
+@Composable
+fun HistoryHuntTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    content: @Composable () -> Unit
+) {
+    val colorScheme = if (darkTheme) DarkColorScheme else LightColorScheme
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        content = content
+    )
+}
