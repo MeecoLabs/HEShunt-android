eb61bfc02d4345fa2ba065eb1d39738970dc61f4
Dustin Steiner <124467+alopix@users.noreply.github.com>
2026-06-23 09:17:19 +0100

    optimise release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,13 @@ android {
 
     buildTypes {
         release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+            )
+
             optimization {
                 enable = false
             }
